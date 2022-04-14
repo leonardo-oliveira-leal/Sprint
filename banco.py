cc481625c52969b3f75c640edea1fa60fd43382b
leonardo-oliveira-leal <leonardo.leal@bandtec.com.br>
2022-04-13 21:36:28 -0300

    conexão com o docker na ec2

--- banco.py
+++ banco.py
@@ -1,9 +1,9 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="Leoleal5020",
+  host="54.166.197.65",
+  user="Leonardo",
+  password="sprint123",
   database="python"
 )
 print(mydb)
